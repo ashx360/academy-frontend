9d0341d8720bf19109b8d6c16db5476f85fb9ca9
ashx360 <mirzaldipasha766@gmail.com>
2025-10-04 21:56:01 +0700

    Update styles and config files

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,3 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
 })
-module.exports = {
-  theme: {
-    extend: {
-      animation: {
-        loading: 'loading 3s linear forwards',
-      },
-      keyframes: {
-        loading: {
-          '0%': { transform: 'translateX(-100%)' },
-          '100%': { transform: 'translateX(0)' },
-        },
-      },
-    },
-  },
-  plugins: [],
-};
